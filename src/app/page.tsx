4783cad9dcb4b4c1d056879808b673a5bd7293fd
Asma Belaidi <asma.belaidi40@gmail.com>
2023-07-31 20:02:29 +0200

    home page: upper buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,33 @@
 "use client"
+import React, { useState } from "react";
+import { Box, Button, Container, Stack, styled, Typography } from "@mui/material";
+import { Edit, FilterList } from "@mui/icons-material";
 
-import Navbar from "@/components/navbar";
-import React from "react";
-import { createTheme, ThemeProvider } from "@mui/material";
+  // -------- Styled Components -------- 
+  
+  const Title = styled(Typography)({
+    fontWeight: "bold"
+  });
+
 export default function Home() {
- const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#003366",
-      light: "#CBD6E2",
-      dark: "#708FAF",
-    }
-  }, 
-  typography:{
-     fontFamily: 'Quicksand',
-     fontWeightLight: 300,
-     fontWeightRegular: 400,
-     fontWeightMedium: 500, 
-     fontWeightBold: 700, 
-  }
- })
+    // ------------ States ----------------
+   const [prodNumber, setProdNumber] = useState(20000)
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-    </ThemeProvider>
+       <Container>
+         <Stack direction="row" gap={2} sx={{mt:"7rem"}}>
+            <Title variant="h5" color="primary">
+                Products <Edit />
+            </Title>
+            <Typography variant="subtitle2" color="primary.dark"
+               sx={{padding: "0.5rem"}}
+            >
+              {prodNumber} {prodNumber > 1 ?  "Products":  "Product" }  
+            </Typography>
+         </Stack>
+         <Stack direction="row" mt={2}>
+          <Button variant="outlined" sx={{fontWeight: "bold"}} startIcon={<FilterList />}>Filter</Button>
+         </Stack>
+       </Container>
+  
   );
 }
