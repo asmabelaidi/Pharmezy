4b6437ed6f06d7a91ce9c9556e5b6dfe74d75db6
Asma Belaidi <asma.belaidi40@gmail.com>
2023-07-31 19:24:24 +0200

    Navbar : design enhancement

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,49 +9,49 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Navbar() {
   return (
-    <AppBar position="fixed" color ="inherit" sx={{borderBottom: "2px solid #F6F8FB"}}>
-    <Container maxWidth="xl">
-      <Toolbar disableGutters>
-        <Healing  color= "primary" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-        <Typography 
-          color= "primary"
-          variant="h6"
-          noWrap
-          fontWeight= 'fontWeightBold'
-          component="a"
-          href="/"
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            letterSpacing: '.3rem',
-            textDecoration: 'none',
-            textTransform: 'uppercase',
-          }}
-        >
-          Pharmezy
-        </Typography>
+    <AppBar position="fixed"  color ="inherit" sx={{borderBottom: "2px solid #F6F8FB"}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Healing  color= "primary" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography 
+            color= "primary"
+            variant="h6"
+            noWrap
+            fontWeight= 'bold'
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              letterSpacing: '.3rem',
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+            }}
+          >
+            Pharmezy
+          </Typography>
 
-        <Box sx={{display: { xs: 'none', md: 'flex' }, margin: 'auto' }}>
-          {pages.map((page) => (
-            <Button
-              key={page}
-              sx={{ ml:4 , my: 2, color: 'white', display: 'block' }}
-            >
-              <Typography fontWeight= 'fontWeightBold' color= "primary" textTransform="capitalize">{page}</Typography>
+          <Box sx={{display: { xs: 'none', md: 'flex' }, margin: 'auto' }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ ml:4 , my: 2, color: 'white', display: 'block' }}
+              >
+                <Typography fontWeight= 'fontWeightBold' color= "primary" textTransform="capitalize">{page}</Typography>
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+
+            <Button color="inherit" sx={{borderLeft:" 2px solid gray", borderRadius: "0px" }}>
+              <Login fontWeight= 'fontWeightBold' color= "primary" />
             </Button>
-          ))}
-        </Box>
-
-        <Box sx={{ flexGrow: 0 }}>
-
-           <Button color="inherit" sx={{borderLeft:" 2px solid gray", borderRadius: "0px" }}>
-             <Login fontWeight= 'fontWeightBold' color= "primary" />
-           </Button>
-         
-        </Box>
-      </Toolbar>
-    </Container>
-  </AppBar>
+          
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
